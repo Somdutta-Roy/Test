2533f7cbd486fc29422abe29848f0543902c0344
Somdutta Roy <sansomstar84@gmail.com>
2020-11-20 13:14:01 -0500

    FinalTest

--- Program.cs
+++ Program.cs
@@ -2,42 +2,61 @@
 
 namespace ConsoleApp4
 {
-    class Program
+    public class Program
     {
+         
         static void Main(string[] args)
         {
-            var Obj = new Object[2];
-            Obj[0] = new Adult("Som", "Roy");
-            Obj[1] = new Child("Ria", " Dey");
-            Console.WriteLine(Obj[0].Fir)
 
-            foreach(Object item in Obj)
+            Identity[] array = new Identity[2];
+            array[0] = new Adult("Som", "Roy");
+                  array[1]= new Child("Ria", "Dey");
+
+            foreach (Identity item in array)
             {
-                Console.WriteLine("Name:" + );
+                Console.WriteLine(item.GetName());
             }
-
-            Console.WriteLine("Hello World!");
+            
         }
-
-        public class Adult
-        { 
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-         public  Adult(string Firstname,string Lastname)
-        {
-            Firstname = Firstname;
-            Lastname = Lastname;
-
-            }
     }
-    public class Child
+
+    public interface Identity
     {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-      public  Child(string Firstname, string Lastname)
+        public  string GetName();
+    }
+        public class Adult:Identity
         {
-            Firstname = Firstname;
-            Lastname = Lastname;
+       
+        public string Firstname { get; }
+        public string Lastname { get; }
+        public Adult(string Fname, string Lname)
+              {
+                Firstname = Fname;
+                Lastname = Lname;
+              }
+
+        public string GetName()
+        {
+            string name = Firstname + Lastname;
+            return name;
         }
     }
-}
+        public class Child : Identity
+        {
+            public string Firstname { get; }
+            public string Lastname { get;  }
+            public Child(string Firstname, string Lastname)
+            {
+               this.Firstname = Firstname;
+               this.Lastname = Lastname;
+            }
+
+        public  string GetName()
+        {
+            string name = Firstname + Lastname;
+            return name;
+        }
+
+    }
+    }
+
